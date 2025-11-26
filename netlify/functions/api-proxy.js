85fb84a0ff514c6ec526e7e72c8f591ed72a0b05
RyGuyLabs <ryan@ryguylabs.com>
2025-11-26 15:00:02 -0500

    Update api-proxy.js

--- netlify/functions/api-proxy.js
+++ netlify/functions/api-proxy.js
@@ -536,7 +536,7 @@ if (feature === 'tts') {
             responseModalities: ["AUDIO"],
             speechConfig: {                        
                 voiceConfig: {
-                    prebuiltVoiceConfig: { voiceName: "Iapetus" }
+                    prebuiltVoiceConfig: { voiceName: "Charon" }
                 }
             }
         }
