e8e26821905b0867da8ae22a2265d1affcf0e0ea
RyGuyLabs <ryan@ryguylabs.com>
2025-12-04 08:26:46 -0500

    Update dashboard.js

--- netlify/functions/dashboard.js
+++ netlify/functions/dashboard.js
@@ -14,7 +14,7 @@ export async function handler(event, context) {
         return {
             statusCode: 200,
             headers: {
-                "Access-Control-Allow-Origin": "https://www.ryguylabs.com",
+                "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Headers": "Content-Type",
                 "Access-Control-Allow-Methods": "POST, OPTIONS",
             },
