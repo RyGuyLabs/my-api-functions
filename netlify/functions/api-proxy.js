f6415d6580a82fdc2705bf63aa094c717f7359b5
RyGuyLabs <ryan@ryguylabs.com>
2025-12-10 08:10:22 -0500

    Update api-proxy.js

--- netlify/functions/api-proxy.js
+++ netlify/functions/api-proxy.js
@@ -467,8 +467,8 @@ exports.handler = async (event, context) => {
         throw new Error('Missing "imagePrompt" for image generation.');
     }
 
-    const IMAGEN_MODEL = "imagen-2.0-generate-002"; 
-    const IMAGEN_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/${IMAGEN_MODEL}:generateImages?key=${GEMINI_API_KEY}`;            
+    const IMAGEN_MODEL = "imagen-3.0-generate-002"; 
+const IMAGEN_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/${IMAGEN_MODEL}:generateImages?key=${GEMINI_API_KEY}`;            
     
     const imagenPayload = {
         model: IMAGEN_MODEL, 
