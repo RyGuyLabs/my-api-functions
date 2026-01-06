23f710f682a661c73ba2808808d215ff5331aacd
RyGuyLabs <ryan@ryguylabs.com>
2026-01-06 12:07:50 -0500

    Update api-proxy.js

--- netlify/functions/api-proxy.js
+++ netlify/functions/api-proxy.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch').default || require('node-fetch');
+const fetch = require('node-fetch');
 
 const SQUARESPACE_TOKEN = process.env.SQUARESPACE_ACCESS_TOKEN;
 const FIRESTORE_KEY = process.env.DATA_API_KEY;
