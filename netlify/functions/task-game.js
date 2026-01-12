5e87692918f451fff62f99f9d4c052a7a5c6931a
RyGuyLabs <ryan@ryguylabs.com>
2026-01-12 14:27:11 -0500

    Update task-game.js

--- netlify/functions/task-game.js
+++ netlify/functions/task-game.js
@@ -1,5 +1,5 @@
 const { db } = require('./firebaseClient'); 
-const { collection, addDoc } = require('firebase-admin/firestore'); 
+const { collection, addDoc } = require('firebase-admin/firestore');
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const GEMINI_API_KEY = process.env.SUM_GAME_KEY;
