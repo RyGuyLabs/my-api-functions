f0c7c359c863e130bf2a29455c6229534819ab30
RyGuyLabs <ryan@ryguylabs.com>
2026-01-14 10:35:07 -0500

    Update task-game.js

--- netlify/functions/task-game.js
+++ netlify/functions/task-game.js
@@ -3,7 +3,7 @@ const { collection, addDoc } = require('firebase-admin/firestore');
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const GEMINI_API_KEY = process.env.SUM_GAME_KEY;
-const LLM_MODEL = 'gemini-pro';
+const LLM_MODEL = 'models/gemini-1.0-pro';
 const PROJECT_ID = process.env.FIRESTORE_PROJECT_ID;
 
 const FIRESTORE_BASE_URL =
