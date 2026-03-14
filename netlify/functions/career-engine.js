1fbe145972e2aa7d94cf070da97452e64eb05500
RyGuyLabs <ryan@ryguylabs.com>
2026-03-14 10:01:04 -0400

    Update career-engine.js

--- netlify/functions/career-engine.js
+++ netlify/functions/career-engine.js
@@ -19,11 +19,11 @@ function isRateLimited(ip) {
 
 exports.handler = async (event, context) => {
     const headers = {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        "Access-Control-Allow-Methods": "POST, OPTIONS",
-        "Content-Type": "application/json"
-    };
+    "Access-Control-Allow-Origin": "https://www.ryguylabs.com",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    "Access-Control-Allow-Methods": "POST, OPTIONS",
+    "Content-Type": "application/json"
+};
 
     if (event.httpMethod === "OPTIONS") {
         return { statusCode: 200, headers, body: "OK" };
