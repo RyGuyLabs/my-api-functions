02af2b3fd71d8cea3745170c6774d8154bbe8f64
RyGuyLabs <ryan@ryguylabs.com>
2026-05-21 07:46:34 -0400

    Update nexus-graph.js

--- netlify/functions/nexus-graph.js
+++ netlify/functions/nexus-graph.js
@@ -1,11 +1,12 @@
 exports.handler = async (event) => {
 
     // CORS HEADERS
-    const headers = {
+    const CORS_HEADERS = {
     "Access-Control-Allow-Origin": "https://www.ryguylabs.com",
-    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
     "Access-Control-Allow-Methods": "POST, OPTIONS",
-    "Access-Control-Max-Age": "86400"
+    "Access-Control-Max-Age": "86400",
+    "Vary": "Origin"
 };
 
     // HANDLE PREFLIGHT
