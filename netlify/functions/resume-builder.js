63318f36207d54ac64ea5986166e3369c304f2af
RyGuyLabs <ryan@ryguylabs.com>
2026-07-12 17:06:18 -0400

    Update resume-builder.js

--- netlify/functions/resume-builder.js
+++ netlify/functions/resume-builder.js
@@ -160,7 +160,7 @@ const geminiUrl = `https://generativelanguage.googleapis.com/v1beta/models/${mod
         try {
           const parsedResult = JSON.parse(responseBody);
           
-          if (res.statusCode !== 200) {
+          if ([429,500,503].includes(res.statusCode)) {
             console.error('[Gemini API Error]', responseBody);
             return resolve({
               statusCode: res.statusCode,
