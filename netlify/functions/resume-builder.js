90ad32c2f13ec9ac7386a5ece3758556d338ce16
RyGuyLabs <ryan@ryguylabs.com>
2026-07-13 14:27:41 -0400

    Update resume-builder.js

--- netlify/functions/resume-builder.js
+++ netlify/functions/resume-builder.js
@@ -12,15 +12,6 @@ if (!admin.apps.length) {
 }
 
 exports.handler = async (event, context) => {
-  if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
-    })
-  });
-}
   // Setup standard headers for CORS & JSON output
   const allowedOrigins = [
   'https://www.ryguylabs.com',
